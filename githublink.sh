9a54a67de735dce2850b5eb2b572fb191be059fc
mjerta <maarten.postma1000@gmail.com>
2024-06-15 21:52:32 +0200

    Added new script

---
+++ githublink.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Get the repository URL in JSON format
+repo_info=$(gh repo view --json url)
+
+# Extract the URL using jq (install jq if you don't have it)
+repo_url=$(echo "$repo_info" | jq -r '.url')
+
+# Get the current branch name
+current_branch=$(git rev-parse --abbrev-ref HEAD)
+
+# Combine the URL with the current branch name
+full_url="${repo_url}/tree/${current_branch}"
+
+# Output the final URL
+echo "$full_url"
+
